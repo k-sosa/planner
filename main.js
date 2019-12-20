aa0c186e003908ae10b265c068565c2a57bdb0de
Karen Montes <karen.montes04@gmail.com>
2019-12-19 17:59:08 -0700

    table is set up in html but javascript functions not working

--- main.js
+++ main.js
@@ -1,2 +1,47 @@
-moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(moment())
+const date1 = moment().format("LLLL")
+const dateHeader = $("<hd1>");
+dateHeader.addClass("header-color");
+dateHeader.attr("date-data", date1);
+dateHeader.text(date1);
+$(".head").append(date1);
+
+const userInput = $(".user-input");
+const savedNote = $(".saved-note");
+const displayNote = userInput.value;
+
+renderLastRegistered();
+
+
+function displayMessage(type, message) {
+  savedNote.textContent = message;
+  savedNote.setAttribute("id", type);
+}
+
+function renderLastRegistered() {
+  const appointments = localStorage.getItem("todos");
+
+  userInput.textContent = todos;
+
+}
+
+$(".saveLocalStorage")click(function(nothing) {
+  nothing.preventDefault();
+  console.log(nothing)
+
+  const userInput = $(".user-input").value
+
+  if (userInput === "") {
+    displayMessage("error", "Note cannot be blank");
+  } else {
+    displayMessage("success", "Success");
+
+    localStorage.setItem("todos", appointments);
+    
+    renderLastRegistered();
+  }
+});
+
+
+
+
+
